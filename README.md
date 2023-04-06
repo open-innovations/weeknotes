bc200f211b9405f2d266a139edc1304dfb30ff23
Giles Dring <giles@dringtech.com>
2023-04-06 17:00:19 +0100

    Updating archetype

--- README.md
+++ README.md
@@ -5,3 +5,13 @@ This is the repo holding weeknotes for the Open Innovations team.
 You can find them online at https://weeknotes.open-innovations.org/
 
 Watch this space...
+
+## Creating a new blank post
+
+Run this:
+
+```sh
+deno task create
+```
+
+THis will create a new blank weeknote for the forthcoming Friday with the correct date and title, based on the [weeknote archetype](src/_archetypes/weeknote.ts).
--- src/_archetypes/weeknote.ts
+++ src/_archetypes/weeknote.ts
@@ -1,8 +1,19 @@
+function getNextFriday(dateString?: string = undefined) {
+  const nextFriday = dateString ? new Date(dateString) : new Date();
+  // Zero out the hours
+  nextFriday.setUTCHours(0, 0, 0, 0);
+  // Set to the next Friday
+  nextFriday.setUTCDate(nextFriday.getUTCDate() + (12 - nextFriday.getUTCDay()) % 7);
+  return nextFriday.toISOString().split('T')[0];
+}
+
 export default function (week_ending: string) {
   const index = Array.from(Deno.readDirSync('src/weeknotes'))
     .filter(x => x.isFile && x.name.endsWith('.md'))
     .length + 1;
-  const date = new Date(Date.parse(week_ending)).toISOString().split('T')[0];
+  const date = getNextFriday(week_ending);
+
+  console.log(`Creating weeknote for ${date}...`);
 
   return {
     path: `/weeknotes/${ date }.md`,
@@ -10,6 +21,7 @@ export default function (week_ending: string) {
       week_ending: date,
       draft: true,
       title: `Weeknotes #${ index }`,
+      description: "TODO Put a broad summary of the contents here. Used in social media links etc. No more than 2 lines. It's for this https://ahrefs.com/blog/seo-meta-tags/#meta-description"
     },
   };
 }
